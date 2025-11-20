0f7735cdb3c61e63daa272ff87f73b21c6194e88
rumi <aliazgorrumi@gmail.com>
2025-11-20 16:37:06 +0600

    deployment fix

---
+++ src/app/studio/[[...tool]]/StudioComponent.tsx
@@ -0,0 +1,7 @@
+"use client"
+import { NextStudio } from 'next-sanity/studio'
+import config from '../../../../sanity.config'
+
+export default function StudioComponent() {
+  return <NextStudio config={config} />
+}
